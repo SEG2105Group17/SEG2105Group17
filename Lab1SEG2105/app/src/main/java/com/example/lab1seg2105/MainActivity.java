500cdd071ac2616885adb251c4e570a50bfed015
Daniel Andreev <andreevdan@gmail.com>
2021-09-26 13:52:48 -0400

    Buttons Update

--- Lab1SEG2105/app/src/main/java/com/example/lab1seg2105/MainActivity.java
+++ Lab1SEG2105/app/src/main/java/com/example/lab1seg2105/MainActivity.java
@@ -3,8 +3,39 @@ package com.example.lab1seg2105;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button one;
+    private Button two;
+    private Button three;
+    private Button four;
+    private Button five;
+    private Button six;
+    private Button seven;
+    private Button eight;
+    private Button nine;
+    private Button zero;
+    private Button add;
+    private Button sub;
+    private Button mul;
+    private Button div;
+    private Button mod;
+    private Button dec;
+    private Button sin;
+    private Button cos;
+    private Button tan;
+    private Button pi;
+    private Button exp;
+    private Button sqrt;
+    private Button e;
+    private Button clr;
+    private Button del;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
