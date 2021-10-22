4e8a4f9551b1ef409f7f8b188af2af297a90f7da
Daniel Andreev <andreevdan@gmail.com>
2021-10-21 23:42:51 -0400

    user

--- app/src/main/java/com/example/fall2021profejct1/MemberLoginPage.java
+++ app/src/main/java/com/example/fall2021profejct1/MemberLoginPage.java
@@ -2,6 +2,7 @@ package com.example.fall2021profejct1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -17,5 +18,10 @@ public class MemberLoginPage extends AppCompatActivity {
         setContentView(R.layout.memberlogin_activity);
 
         memberTitleTextView = findViewById(R.id.memberTitleTextView);
+
+
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("message_key");
+        memberTitleTextView.setText("Welcome "+ str + " You are logged in as member!");
     }
 }
