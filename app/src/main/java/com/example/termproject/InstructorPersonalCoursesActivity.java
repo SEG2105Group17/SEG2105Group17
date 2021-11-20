d921b267768919ad181d5eeff1a777daf9c1f291
Natasha Tremblay Daszkiewicz <90850975+ntrem071@users.noreply.github.com>
2021-11-20 14:38:36 -0500

    Update InstructorPersonalCoursesActivity.java

--- app/src/main/java/com/example/termproject/InstructorPersonalCoursesActivity.java
+++ app/src/main/java/com/example/termproject/InstructorPersonalCoursesActivity.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 //delete crashes when new item selected after used/
 // error with txtClassInfo list on click display when edit used (back and return seems to make it better)
@@ -93,7 +94,7 @@ public class InstructorPersonalCoursesActivity extends AppCompatActivity {
                 txtIID.setText("Course not offered by gym");
                 return;
             }
-
+            String description= check.find(name).description;
             int capacity, difficulty;
             try {
                 capacity = Integer.parseInt(editICapacity.getText().toString().trim());
@@ -103,8 +104,11 @@ public class InstructorPersonalCoursesActivity extends AppCompatActivity {
                 txtIID.setText("Please enter an integer value for capacity and difficulty");
                 return;
             }
-            String description= check.find(name).description;
-            String date =editIDate.getText().toString().trim();
+            if(!isDay(editIDate.getText().toString())){
+                txtIID.setText("Please enter valid day of week for date");
+                return;
+            }
+            String date =editIDate.getText().toString().trim().toLowerCase();
             String time =editITime.getText().toString().trim();
 
             ClassClass newCourse = new ClassClass(name, description, date, time, difficulty,capacity,username);
@@ -214,6 +218,17 @@ public class InstructorPersonalCoursesActivity extends AppCompatActivity {
         Intent intent= new Intent(this, InstructorSelectActivity.class);
         intent.putExtra("username", username);
         startActivity(intent);
+    }
+
+    //for newProduct
+    public boolean isDay(String day){
+        String[] days = {"monday","tuesday","wednesday","thursday","friday","saturday","sunday"};
+        boolean flag =false;
+        day = day.trim().toLowerCase();
+        for (int i=0;i<days.length;i++){
+            if (day!=null&&day.equals(days[i])){ flag=true;}
+        }
+        return flag;
     }
 
 
