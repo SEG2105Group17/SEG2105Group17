eb9e44bf4469e0f2b0f181ec749dd63f4b2d6ead
Daniel Andreev <andreevdan@gmail.com>
2021-11-19 19:05:56 -0500

    test update

--- app/src/main/java/com/example/termproject/ClassClassTest.java
+++ app/src/main/java/com/example/termproject/ClassClassTest.java
@@ -24,39 +24,105 @@ public class ClassClassTest {
         assertThat(noInput.capacity()).isEqualTo(-1);
 
         // test for 3 input
-        assertThat(noInput.getName()).isEqualTo("Zen");
-        assertThat(noInput.getDescription()).isEqualTo("Vibes");
+        assertThat(with3Input.getName()).isEqualTo("Zen");
+        assertThat(with3Input.getDescription()).isEqualTo("Vibes");
+        assertThat(with3Input.getDate()).isEqualTo("");
+        assertThat(with3Input.getTime()).isEqualTo("");
+        assertThat(with3Input.id()).isEqualTo(123);
+        assertThat(with3Input.Difficulty()).isEqualTo(-1);
+        assertThat(with3Input.capacity()).isEqualTo(-1);
+
+        // test for 2 input
+        assertThat(with2Input.getName()).isEqualTo("Zen");
+        assertThat(with2Input.getDescription()).isEqualTo("Vibes");
+        assertThat(with2Input.getDate()).isEqualTo("");
+        assertThat(with2Input.getTime()).isEqualTo("");
+        assertThat(with2Input.id()).isEqualTo(-1);
+        assertThat(with2Input.Difficulty()).isEqualTo(-1);
+        assertThat(with2Input.capacity()).isEqualTo(-1);
+
+        // test for 1 input
+        assertThat(with1Input.getName()).isEqualTo("Zen");
+        assertThat(with1Input.getDescription()).isEqualTo("");
+        assertThat(with1Input.getDate()).isEqualTo("");
+        assertThat(with1Input.getTime()).isEqualTo("");
+        assertThat(with1Input.id()).isEqualTo(-1);
+        assertThat(with1Input.Difficulty()).isEqualTo(-1);
+        assertThat(with1Input.capacity()).isEqualTo(-1);
+
+        // test for 6 input
+        assertThat(with6Input.getName()).isEqualTo("Zen");
+        assertThat(with6Input.getDescription()).isEqualTo("Vibes");
+        assertThat(with6Input.getDate()).isEqualTo("1/4/2022");
+        assertThat(with6Input.getTime()).isEqualTo("3:33");
+        assertThat(with6Input.id()).isEqualTo(-1);
+        assertThat(with6Input.Difficulty()).isEqualTo(2);
+        assertThat(with6Input.capacity()).isEqualTo(500);
+    }
+
+    public void testChanges() {
+        noInput.changeID(1814);
+        with3Input.changeID(1814);
+        with2Input.changeID(1814);
+        with1Input.changeID(1814);
+        with6Input.changeID(1814);
+
+        noInput.changeName("Hell");
+        with3Input.changeName("Hell");
+        with2Input.changeName("Hell");
+        with1Input.changeName("Hell");
+        with6Input.changeName("Hell");
+
+        noInput.changeDescription("Not-A-Vibe");
+        with3Input.changeDescription("Not-A-Vibe");
+        with2Input.changeDescription("Not-A-Vibe");
+        with1Input.changeDescription("Not-A-Vibe");
+        with6Input.changeDescription("Not-A-Vibe");
+
+        // test for no input
+        assertThat(noInput.getName()).isEqualTo("Hell");
+        assertThat(noInput.getDescription()).isEqualTo("Not-A-Vibe");
+        assertThat(noInput.getDate()).isEqualTo("");
+        assertThat(noInput.getTime()).isEqualTo("");
+        assertThat(noInput.id()).isEqualTo(-1);
+        assertThat(noInput.Difficulty()).isEqualTo(-1);
+        assertThat(noInput.capacity()).isEqualTo(1814);
+
+        // test for 3 input
+        assertThat(noInput.getName()).isEqualTo("Hell");
+        assertThat(noInput.getDescription()).isEqualTo("Not-A-Vibe");
         assertThat(noInput.getDate()).isEqualTo("");
         assertThat(noInput.getTime()).isEqualTo("");
         assertThat(noInput.id()).isEqualTo(123);
         assertThat(noInput.Difficulty()).isEqualTo(-1);
-        assertThat(noInput.capacity()).isEqualTo(-1);
+        assertThat(noInput.capacity()).isEqualTo(1814);
 
         // test for 2 input
-        assertThat(noInput.getName()).isEqualTo("Zen");
-        assertThat(noInput.getDescription()).isEqualTo("Vibes");
+        assertThat(noInput.getName()).isEqualTo("Hell");
+        assertThat(noInput.getDescription()).isEqualTo("Not-A-Vibe");
         assertThat(noInput.getDate()).isEqualTo("");
         assertThat(noInput.getTime()).isEqualTo("");
         assertThat(noInput.id()).isEqualTo(-1);
         assertThat(noInput.Difficulty()).isEqualTo(-1);
-        assertThat(noInput.capacity()).isEqualTo(-1);
+        assertThat(noInput.capacity()).isEqualTo(1814);
 
         // test for 1 input
-        assertThat(noInput.getName()).isEqualTo("Zen");
-        assertThat(noInput.getDescription()).isEqualTo("");
+        assertThat(noInput.getName()).isEqualTo("Hell");
+        assertThat(noInput.getDescription()).isEqualTo("Not-A-Vibe");
         assertThat(noInput.getDate()).isEqualTo("");
         assertThat(noInput.getTime()).isEqualTo("");
         assertThat(noInput.id()).isEqualTo(-1);
         assertThat(noInput.Difficulty()).isEqualTo(-1);
-        assertThat(noInput.capacity()).isEqualTo(-1);
+        assertThat(noInput.capacity()).isEqualTo(1814);
 
         // test for 6 input
-        assertThat(noInput.getName()).isEqualTo("Zen");
-        assertThat(noInput.getDescription()).isEqualTo("Vibes");
+        assertThat(noInput.getName()).isEqualTo("Hell");
+        assertThat(noInput.getDescription()).isEqualTo("Not-A-Vibe");
         assertThat(noInput.getDate()).isEqualTo("1/4/2022");
         assertThat(noInput.getTime()).isEqualTo("3:33");
         assertThat(noInput.id()).isEqualTo(-1);
         assertThat(noInput.Difficulty()).isEqualTo(2);
-        assertThat(noInput.capacity()).isEqualTo(500);
+        assertThat(noInput.capacity()).isEqualTo(1814);
     }
+
 }
