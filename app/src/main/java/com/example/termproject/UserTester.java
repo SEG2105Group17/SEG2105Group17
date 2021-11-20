d82b65899d2c258f61e56309b7045ffb77f73a9d
Daniel Andreev <andreevdan@gmail.com>
2021-11-19 20:40:09 -0500

    Unit test user testing completed

---
+++ app/src/main/java/com/example/termproject/UserTester.java
@@ -0,0 +1,62 @@
+package com.example.termproject;
+
+import com.google.common.truth.Truth.assertThat;
+import org.junit.Test;
+
+public class UserTester {
+    User testUserblank = new User();
+    User testUser3Input = new User(123, "avrage", "joe");
+    User testUser2Input = new User("avrage", "joe");
+
+    @Test
+    /** Chewcking constructors */
+    public void testUserConstructor() {
+
+        // testing with black constructor
+        assertThat(testUserblank.getID()).isEqualTo(-1);
+        assertThat(testUserblank.getUserName()).isEqualTo("");
+        assertThat(testUserblank.getUserType()).isEqualTo("");
+
+        // testing with 3 inputs constructor
+        assertThat(testUser3Input.getID()).isEqualTo(123);
+        assertThat(testUser3Input.getUserName()).isEqualTo("avrage");
+        assertThat(testUser3Input.getUserType()).isEqualTo("joe");
+
+        // testing with 2 inputs constructor
+        assertThat(testUser2Input.getID()).isEqualTo(-1);
+        assertThat(testUser2Input.getUserName()).isEqualTo("avrage");
+        assertThat(testUser2Input.getUserType()).isEqualTo("joe");
+
+    }
+
+    @Test
+    /** Check the setter methods */
+    public void testChanges() {
+        testUserblank.setID(456);
+        testUser3Input.setID(456);
+        testUser2Input.setID(456);
+
+        testUserblank.setUserName("HELP");
+        testUser3Input.setUserName("HELP");
+        testUser2Input.setUserName("HELP");
+
+        testUserblank.setUserType("GOD");
+        testUser3Input.setUserType("GOD");
+        testUser2Input.setUserType("GOD");
+
+        // testing with black constructor
+        assertThat(testUserblank.getID()).isEqualTo(456);
+        assertThat(testUserblank.getUserName()).isEqualTo("HELP");
+        assertThat(testUserblank.getUserType()).isEqualTo("GOD");
+
+        // testing with 3 inputs constructor
+        assertThat(testUser3Input.getID()).isEqualTo(456);
+        assertThat(testUser3Input.getUserName()).isEqualTo("HELP");
+        assertThat(testUser3Input.getUserType()).isEqualTo("GOD");
+
+        // testing with 2 inputs constructor
+        assertThat(testUser2Input.getID()).isEqualTo(456);
+        assertThat(testUser2Input.getUserName()).isEqualTo("HELP");
+        assertThat(testUser2Input.getUserType()).isEqualTo("GOD");
+    }
+}
