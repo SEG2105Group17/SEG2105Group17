b4ce60815f676bf8a46bf1cb19bc1895d0c29426
Natasha Tremblay Daszkiewicz <90850975+ntrem071@users.noreply.github.com>
2021-11-21 15:14:41 -0500

    commented out testcases (AssertThat error)

--- app/src/main/java/com/example/termproject/InstructorLoginActivity.java
+++ app/src/main/java/com/example/termproject/InstructorLoginActivity.java
@@ -1,13 +1,13 @@
 package com.example.termproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.databases.MyDBHandler;
 
@@ -61,11 +61,4 @@ public class InstructorLoginActivity extends AppCompatActivity {
         passwordBox.setText("");
     }
 
-    public void newInstructor(View view){
-        MyDBHandler dbHandler = new MyDBHandler(this);
-        User user = new User(usernameBox.getText().toString(), "Instructor");
-        dbHandler.addUser(user);
-        usernameBox.setText("");
-        passwordBox.setText("");
-    }
 }
